117ffe58e2b3c17a45e1542ef5930bbf0eec6fa2
Victoria <Victoria.tukisi@gmail.com>
2021-02-12 13:36:33 +0200

    second commit

--- TASK1_APP/src/main/java/files/Resources.java
+++ TASK1_APP/src/main/java/files/Resources.java
@@ -15,7 +15,7 @@ public class Resources {
 	public String InitializeHost() throws IOException {
 		
 		prop = new Properties();
-		FileInputStream fis = new FileInputStream("C:\\Users\\de01\\TASK1_APP\\src\\main\\java\\files\\environment.properties");
+		FileInputStream fis = new FileInputStream("C:\\Workspace\\Automation-Testing-Assessments\\TASK1_APP\\src\\main\\java\\files\\environment.properties");
 		prop.load(fis);				
 		
 		String hostname = prop.getProperty("HOST");
